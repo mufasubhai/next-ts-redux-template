c3a67246fc3b2410698b10a53dee50ba38cb9ad0
adrian <aapodaca@gmail.com>
2022-05-31 11:56:43 -0700

    update template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,61 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+// const IndexPage = () => (
+//   <Layout title="Home | Next.js + TypeScript Example">
+//     <h1>Hello Next.js 👋</h1>
+//     <p>
+//       <Link href="/about">
+//         <a>About</a>
+//       </Link>
+//     </p>
+//   </Layout>
+// )
 
-export default IndexPage
+// export default IndexPage
+
+import { useState } from 'react';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from '../features/counter/counterSlice';
+
+const IndexPage:React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
+  return (
+    <>
+      <h1>Welcome to the greatest app in the world!</h1>
+      <h2>
+        The current number is
+        {count}
+      </h2>
+      <div>
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(Number(e.target.value))}
+          type="number"
+        />
+        <button
+          onClick={() =>        dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment by amount
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
+      </div>
+    </>
+  );
+};
+
+export default IndexPage;
